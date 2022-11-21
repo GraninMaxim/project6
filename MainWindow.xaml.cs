8bf707ed8087713225d524a438a42de304830ab5
podnepry@bk.ru <podnepry@bk.ru>
2022-11-21 13:37:08 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace project6
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Triad triad1;
+        Triad triad2;
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            inputValue1.Text = null;
+            inputValue2.Text = null;
+            inputValue3.Text = null;
+            inputValueA.Text = null;
+            inputValueB.Text = null;
+            inputValueC.Text = null;
+            amount1.Text = null;
+            amount2.Text = null;
+            triad1.ClearTriad();
+            triad2.ClearTriad();
+        }
+
+        private void Enter_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                Compare_Click(compare, null);
+            }
+        }
+
+        private void About_Program_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Разработать операции определения равенства/неравенства " +
+                "чисел true/false. Разработать операции проверки полного равенства/неравенства " +
+                "чисел в триадах (a1,b1,c1) == (a2,b2,c2)", "О программе",
+                MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Compare_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                uint value1 = uint.Parse(inputValue1.Text);
+                uint value2 = uint.Parse(inputValue2.Text);
+                uint value3 = uint.Parse(inputValue3.Text);
+                uint valueA = uint.Parse(inputValueA.Text);
+                uint valueB = uint.Parse(inputValueB.Text);
+                uint valueC = uint.Parse(inputValueC.Text);
+
+                triad1 = new Triad(value1, value2, value3);
+                triad2 = new Triad(valueA, valueB, valueC);
+                if (triad1 == triad2)
+                {
+                    MessageBox.Show("Тройки чисел равны", "Результат", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                }
+                else MessageBox.Show("Тройки чисел не равны", "Результат", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Calculate_and_Compare_Click(object sender, RoutedEventArgs e)
+        {
+            
+            try
+            {
+                uint value1 = uint.Parse(inputValue1.Text);
+                uint value2 = uint.Parse(inputValue2.Text);
+                uint value3 = uint.Parse(inputValue3.Text);
+                uint valueA = uint.Parse(inputValueA.Text);
+                uint valueB = uint.Parse(inputValueB.Text);
+                uint valueC = uint.Parse(inputValueC.Text);
+
+                triad1 = new Triad(value1, value2, value3);
+                triad2 = new Triad(valueA, valueB, valueC);
+
+                amount1.Text = triad1.Amount().ToString();
+                amount2.Text = triad2.Amount().ToString();
+                if (triad1 > triad2) MessageBox.Show("Сумма первой тройки больше суммы второй", "Результат", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                else MessageBox.Show("Сумма первой тройки меньше суммы второй", "Результат", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Triad1_Check_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                uint value1 = uint.Parse(inputValue1.Text);
+                uint value2 = uint.Parse(inputValue2.Text);
+                uint value3 = uint.Parse(inputValue3.Text);
+
+                triad1 = new Triad(value1, value2, value3);
+
+                if (triad1) MessageBox.Show("Числа в первой тройке равны", "Результат", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                else MessageBox.Show("Числа в первой тройке не равны", "Результат", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Triad2_Check_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                uint valueA = uint.Parse(inputValueA.Text);
+                uint valueB = uint.Parse(inputValueB.Text);
+                uint valueC = uint.Parse(inputValueC.Text);
+
+                triad2 = new Triad(valueA, valueB, valueC);
+
+                if (triad2) MessageBox.Show("Числа во второй тройке равны", "Результат", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+                else MessageBox.Show("Числа во второй тройке не равны", "Результат", MessageBoxButton.OK, MessageBoxImage.Asterisk);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
---
+++ Triad.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project6
+{
+    internal class Triad
+    {
+        public Triad(uint number1, uint number2, uint number3)
+        {
+            Number1 = number1;
+            Number2 = number2;
+            Number3 = number3;
+        }
+
+
+        public uint Number1 { get; set; }
+        public uint Number2 { get; set; }
+        public uint Number3 { get; set; }
+
+        public uint Amount()
+        {
+            uint amount = Number1 + Number2 + Number3;
+            return amount;
+        }
+
+        public void SetParams(uint number1, uint number2, uint number3)
+        {
+            Number1 = number1;
+            Number2 = number2;
+            Number3 = number3;
+        }
+
+        public void SetParams(uint number1)
+        {
+            Number1 = number1;
+        }
+
+        public void SetParams(uint number1, uint number2)
+        {
+            Number1 = number1;
+            Number2 = number2;
+        }
+
+
+        public void ClearTriad()
+        {
+            Number1 = 0;
+            Number2 = 0;
+            Number3 = 0;
+        }
+
+        public static bool operator !=(Triad firstTriad, Triad secondTriad)
+        {
+            return firstTriad != secondTriad;
+        }
+
+        public static bool operator ==(Triad firstTriad, Triad secondTriad)
+        {
+            if (firstTriad.Number1 == secondTriad.Number1 && firstTriad.Number2 == secondTriad.Number2 && firstTriad.Number3 == secondTriad.Number3)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static bool operator true(Triad triad)
+        {
+            if (triad.Number1 == triad.Number2 && triad.Number1 == triad.Number3) return true;
+            return false;
+        }
+
+        public static bool operator false(Triad triad)
+        {
+            return triad.Number1 != triad.Number2 || triad.Number1 != triad.Number3;
+        }
+
+        public static bool operator >(Triad firstTriad, Triad secondTriad)
+        {
+            if (firstTriad.Amount() > secondTriad.Amount()) return true;
+            else return false;
+        }
+
+        public static bool operator <(Triad firstTriad, Triad secondTriad)
+        {
+            return firstTriad.Amount() < secondTriad.Amount();
+        }
+    }
+}
